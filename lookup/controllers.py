c84f2f611c8669c7456866444e8cf5b9d442cb40
Edwin Avalos <edwin.a.avalos@gmail.com>
2016-12-11 00:08:28 -0600

    Look at the tests edwin

--- lookup/controllers.py
+++ lookup/controllers.py
@@ -3,7 +3,7 @@ from lxml import html
 import requests, logging
 
 def get_next(request_json):
-    requests.get("https://www.metrotransit.org/NexTripBadge.aspx?stopnumber=1890")
+    page = requests.get("https://www.metrotransit.org/NexTripBadge.aspx?stopnumber=1890")
     tree = html.fromstring(page.content)
     next_due = tree.xpath('//*[@id="NexTripControl1_NexTripResults1_departures"]/div[1]/span[3]/text()')
     return next_due
